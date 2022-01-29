b9059430679b94402dd482164bed254fe960361e
LianXinbo <zeroskylian@gmail.com>
2022-01-29 17:12:23 +0800

    init project

---
+++ C_Learn/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_Learn
+//
+//  Created by Xinbo Lian on 2022/1/29.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
